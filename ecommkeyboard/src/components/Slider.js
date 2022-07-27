b5eef6bdc87d95fe156998cab6b9d5df007b958f
laurag701 <laurangonzalez468@gmail.com>
2022-07-27 01:43:52 -0400

    final changes

--- ecommkeyboard/src/components/Slider.js
+++ ecommkeyboard/src/components/Slider.js
@@ -1,14 +1,19 @@
 import { motion } from "framer-motion";
 import { useRef, useEffect, useState } from "react";
-import images from './images'
 import '../App.css';
-
-
+import images from'./images'
 
 function Slider() {
     const [width, setWidth] = useState(0);
     const carousel = useRef();
-    
+    const images= ['https://rad-palmier-f03c7c.netlify.app/images/keyboard1.webp',
+'https://rad-palmier-f03c7c.netlify.app/images/keyboard2.webp',
+'https://rad-palmier-f03c7c.netlify.app/images/keyboard3.webp',
+'https://rad-palmier-f03c7c.netlify.app/images/keyboard5.jpeg',
+'https://rad-palmier-f03c7c.netlify.app/images/keyboard6.jpeg',
+'https://rad-palmier-f03c7c.netlify.app/images/keyboard8.jpeg',
+'https://rad-palmier-f03c7c.netlify.app/images/keyboard10.webp'];
+
     useEffect(()=>{
         
         setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
@@ -20,9 +25,9 @@ function Slider() {
             <motion.div drag="x" 
             dragConstraints={{right: 0, left: -width}} 
             className="inner-carousel">
-                {images.map(image =>{
+                {images.map((image, key) =>{
                     return(
-                    <motion.div className="item" key={images}>
+                    <motion.div className="item" key={key}>
                         <img className='sizing'src={image} alt="" />
                     </motion.div>
                     );
